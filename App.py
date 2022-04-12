47f207b630aa5330a75435dba5afed54ad1e265c
ifedili <88955481+ifedili@users.noreply.github.com>
2022-04-12 03:06:39 +0100

    Update App.py

--- App.py
+++ App.py
@@ -97,6 +97,13 @@ class InfoPage(GridLayout):
         self.message = Label(halign='center', valign='middle', font_size=30)
         self.message.bind(width=self.update_text_width)
         self.add_widget(self.message)     
+          
+    def update_info(self, message):
+        self.message.text = message
+
+    def update_text_width(self, *_):
+        self.message.text_size = (self.message.width * 0.9, None)       
+                       
                        
                        
                        
