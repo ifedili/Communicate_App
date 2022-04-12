af3b0ed044fb7a538aea71bdaa39fd9471a9b9c3
ifedili <88955481+ifedili@users.noreply.github.com>
2022-04-12 01:15:16 +0100

    Update App.py

--- App.py
+++ App.py
@@ -3,3 +3,20 @@ import sys
 import time
 
 import kivy
+from kivy.app clock import App
+from kivy.clock import clock
+from kivy.core.window import Window
+from kivy.uix.dropdown import Button
+from kivy.uix.dropdown  import Dropdown
+from kivy.uix.gridlayout import Gridlayout
+from kivy.uix.label import Label
+from  kivy.uix.label import ScreenManager, Screen
+from kivy.uix.scrollview import Scrollview
+from kivy.uix.textinput import TextInput
+
+kivy.require('2.0.0')
+import socket_client
+
+class Scrollablel(ScrollView):
+  
+  
